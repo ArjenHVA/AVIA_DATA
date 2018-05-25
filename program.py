41f8982c6db35e4a0958192e60c0e7efaca74f34
ArjenHVA <arjen_peters93@hotmail.com>
2018-05-25 20:51:09 +0200

    Initial commit

---
+++ program.py
@@ -0,0 +1,12 @@
+print('----------------------')
+print('GUESS THAT NUMBER GAME')
+print('----------------------')
+print()
+
+user_text = input('What is your name? ')
+
+greeting = 'Nice to meet you ' + user_text
+
+
+
+
